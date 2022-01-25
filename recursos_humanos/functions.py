8c6b45bd4d9f2c67cffb057cc756a6e62271ed78
Marcos Garcia <marcos.garcia@asoandes.org>
2022-01-25 09:06:35 -0500

    Update functions recursos_humanos add timezone as timezone_pyzt

--- recursos_humanos/functions.py
+++ recursos_humanos/functions.py
@@ -3,7 +3,6 @@ import json
 from num2words import num2words
 from PyPDF2 import PdfFileMerger
 from recursos_humanos.models import *
-from django.utils import timezone
 from pytz import timezone as timezone_pyzt
 settings_time_zone = timezone_pyzt(settings.TIME_ZONE)
 
